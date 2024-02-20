f847b57877487e697cf75eec9c75ac7ce6159bd2
Atakan Şerifoğlu <atakan.serifoglu@ug.bilkent.edu.tr>
2024-02-20 09:31:30 +0300

    Created User model
    Also created models folder.

---
+++ potentialbicfp/src/main/java/com/potential/bic/fp/Models/User.java
@@ -0,0 +1,42 @@
+package com.potential.bic.fp.Models;
+
+// A simple class for Jackson to serialize/deserialize
+public class User {
+    private String name;
+    private int age;
+
+    // Default constructor needed for Jackson deserialization
+    public User() {}
+
+    // Constructor
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+               "name='" + name + '\'' +
+               ", age=" + age +
+               '}';
+    }
+}
+
